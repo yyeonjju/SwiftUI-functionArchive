d3ca8e2684a7828f3f471e39b7af446a8f4295ac
yyeonjju <llilyha123@gmail.com>
2022-04-01 09:27:56 +0900

    Initial Commit

---
+++ functionArchive/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  functionArchive
+//
+//  Created by 하연주 on 2022/04/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ functionArchive/functionArchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  functionArchiveApp.swift
+//  functionArchive
+//
+//  Created by 하연주 on 2022/04/01.
+//
+
+import SwiftUI
+
+@main
+struct functionArchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
